79b0f97ccb54e2f11cace7905afed7bf696a8686
jlsnowflake L <104036026+jlsnowflakel@users.noreply.github.com>
2022-04-25 10:58:26 -0400

    Update stremlit_app.py
    added second text entry

--- stremlit_app.py
+++ stremlit_app.py
@@ -41,4 +41,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruite load List Contains:")
+
+fruit_second_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
+streamlit.write('Thanks for adding ', fruit_second_choice)
 streamlit.dataframe(my_data_rows)
